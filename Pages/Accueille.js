88265d4cf8ff1c440400a543802e431dea3fd91c
Your Name <you@example.com>
2025-03-23 02:33:38 +0000

    first commit

---
+++ Pages/Accueille.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from "react";
+import { View, Image, Animated, StyleSheet } from "react-native";
+
+export default function Acceuille() {
+  const rotation = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 3000, // Temps de rotation (3 secondes ici)
+        useNativeDriver: true,
+      })
+    ).start();
+  }, []);
+
+  const rotateInterpolate = rotation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.Image
+        source={require("../assets/images/ServiGOlogo.png")}
+        style={[styles.image, { transform: [{ rotate: rotateInterpolate }] }]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#5E17EB", // Fond violet
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 300, // Ajuste la taille selon ton image
+    height: 300,
+  },
+});
